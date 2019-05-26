d7ac70b60106093079733f1d37d8cd189475fe9c
Kevin Rose <kevin.rose247@gmail.com>
2019-05-26 16:43:34 +0200

    Updated Script Runtime Version / Started Animation Controller

---
+++ Pest/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+	[SerializeField] Animator animator = null;
+	public float Velocity { get; set; } = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		animator.SetFloat("Velocity", Velocity);
+    }
+}
