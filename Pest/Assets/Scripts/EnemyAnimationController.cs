1f62a6c6de776ba5f8691d8ab6a38989258b7fdd
Kevin Rose <kevin.rose247@gmail.com>
2019-05-28 23:03:40 +0200

    Started first enemy takedown

---
+++ Pest/Assets/Scripts/EnemyAnimationController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAnimationController : MonoBehaviour
+{
+	Animator animator = null;
+	/********************animation hashes**********************/
+	int dieHash = Animator.StringToHash("Die");
+
+	private void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	public void Die()
+	{
+		animator.SetTrigger(dieHash);
+	}
+}
