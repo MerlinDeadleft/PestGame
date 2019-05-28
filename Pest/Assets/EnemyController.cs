4ae5f7408c249df2e417c8ed4c71496968a5a4af
Kevin Rose <kevin.rose247@gmail.com>
2019-05-28 23:43:20 +0200

    small commit to be able to pull

--- Pest/Assets/EnemyController.cs
+++ Pest/Assets/EnemyController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+	[SerializeField] EnemyAnimationController animationController = null;
+
     // Start is called before the first frame update
     void Start()
     {
