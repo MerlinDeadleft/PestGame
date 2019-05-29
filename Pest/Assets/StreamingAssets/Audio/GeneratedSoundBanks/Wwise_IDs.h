6f1a12c3a64d4365fb0af3f80602c1fb4367d657
Lorenz Hertkorn <lorenz.hertkorn@googlemail.com>
2019-05-29 02:00:52 +0200

    Footsteps MC Rat Integration
    Leider noch nicht hörbar, aber eigentlich schon da.

--- Pest/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Pest/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,15 +14,34 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID AMBIENCE_MUSIC_KANALISATION = 169578072U;
+        static const AkUniqueID FOOTSTEP_MC_RAT = 2871201794U;
         static const AkUniqueID TORCHES = 2615352377U;
         static const AkUniqueID WATER = 2654748154U;
     } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace FOOTSTEPS
+        {
+            static const AkUniqueID GROUP = 2385628198U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID STONE = 1216965916U;
+                static const AkUniqueID WATER = 2654748154U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS
+
+    } // namespace SWITCHES
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
         static const AkUniqueID AMBIENCEMUSIC_KANALISATION = 1921015285U;
         static const AkUniqueID AMBIENT_KANALISATION = 310433906U;
+        static const AkUniqueID CHARACTER_MC_RAT = 3283972205U;
     } // namespace BANKS
 
     namespace BUSSES
