8006a2fc0313a3790798a89c4efa43d540adf290
Lorenz Hertkorn <lorenz.hertkorn@googlemail.com>
2019-06-06 16:12:35 +0200

    New Generated Soundbanks
    Soundbanks neu generiert

--- Pest/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Pest/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,14 +14,36 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID AMBIENCE_MUSIC_KANALISATION = 169578072U;
-        static const AkUniqueID FOOTSTEP_MC_RAT = 2871201794U;
-        static const AkUniqueID JUMP_MC_RAT = 154336016U;
+        static const AkUniqueID CLIMBING_MC_RAT_FOOT_L = 651427087U;
+        static const AkUniqueID CLIMBING_MC_RAT_FOOT_R = 651427089U;
+        static const AkUniqueID CLIMBING_MC_RAT_HAND_L = 1737296840U;
+        static const AkUniqueID CLIMBING_MC_RAT_HAND_R = 1737296854U;
+        static const AkUniqueID FOOTSTEP_MC_RAT_L = 2785478423U;
+        static const AkUniqueID FOOTSTEP_MC_RAT_R = 2785478409U;
+        static const AkUniqueID FOOTSTEP_MC_RAT_RUN_L = 3501659381U;
+        static const AkUniqueID FOOTSTEP_MC_RAT_RUN_R = 3501659371U;
+        static const AkUniqueID FOOTSTEP_MC_RAT_SNEAK_L = 2017615138U;
+        static const AkUniqueID FOOTSTEP_MC_RAT_SNEAK_R = 2017615164U;
+        static const AkUniqueID HIDING_MC_RAT_HIDE1_JUMP = 254485058U;
+        static const AkUniqueID JUMP_VOICE_MC_RAT = 1240356301U;
         static const AkUniqueID TORCHES = 2615352377U;
         static const AkUniqueID WATER = 2654748154U;
     } // namespace EVENTS
 
     namespace SWITCHES
     {
+        namespace CLIMBING
+        {
+            static const AkUniqueID GROUP = 3990868544U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID STONE = 1216965916U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace CLIMBING
+
         namespace FOOTSTEPS
         {
             static const AkUniqueID GROUP = 2385628198U;
@@ -35,6 +57,15 @@ namespace AK
             } // namespace SWITCH
         } // namespace FOOTSTEPS
 
+        namespace HEAVY
+        {
+            static const AkUniqueID GROUP = 2732489590U;
+
+            namespace SWITCH
+            {
+            } // namespace SWITCH
+        } // namespace HEAVY
+
         namespace JUMPS
         {
             static const AkUniqueID GROUP = 1071740888U;
@@ -47,6 +78,24 @@ namespace AK
                 static const AkUniqueID WOOD = 2058049674U;
             } // namespace SWITCH
         } // namespace JUMPS
+
+        namespace MELEE
+        {
+            static const AkUniqueID GROUP = 3208241451U;
+
+            namespace SWITCH
+            {
+            } // namespace SWITCH
+        } // namespace MELEE
+
+        namespace RANGED
+        {
+            static const AkUniqueID GROUP = 3686152272U;
+
+            namespace SWITCH
+            {
+            } // namespace SWITCH
+        } // namespace RANGED
 
     } // namespace SWITCHES
 
