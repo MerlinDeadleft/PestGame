1e30ef72aa892e0193e24c190b85c4649d2a877e
Kevin Rose <kevin.rose247@gmail.com>
2019-06-09 19:57:27 +0200

    Tweaked some vignette stuff

--- Pest/Assets/Scripts/VignetteController.cs
+++ Pest/Assets/Scripts/VignetteController.cs
@@ -2,14 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
+using Cinemachine;
 
 public class VignetteController : MonoBehaviour
 {
 	PostProcessVolume postProcessVolume = null;
 	Vignette vignette = null;
 	[SerializeField] Transform playerModel = null;
+	[SerializeField] Transform VignetteCenter = null;
 	[SerializeField] PlayerController playerController = null;
 	[SerializeField] float maxDistanceToLight = 10.0f;
+	[SerializeField, Range(0.0f, 1.0f)] float maxVignetteIntensity = 10.0f;
+	[SerializeField] float cameraMinDistance = 4.0f;
+	float defaultCameraDistance = 0.0f;
+	bool zoom = false;
+	CinemachineFramingTransposer framingTransposer = null;
+	[SerializeField] float zoomSpeed = 1.0f;
+	[SerializeField] float reverseZoomSpeed = 1.0f;
+	[SerializeField] float waitForZoomTime = 1.0f;
+	float waitForZoomTimer = 0.0f;
 
 	List<Light> lights = new List<Light>();
 	[SerializeField] List<Light> contributingLight = new List<Light>();
@@ -29,16 +40,43 @@ public class VignetteController : MonoBehaviour
 				lights.Add(light);
 			}
 		}
+
+		StartCoroutine(Initialize());
+	}
+
+	IEnumerator Initialize()
+	{
+		yield return new WaitForEndOfFrame(); // need to wait for a frame for Cinemachine to init
+
+		framingTransposer = Camera.main.GetComponent<CinemachineBrain>().
+			ActiveVirtualCamera.VirtualCameraGameObject.GetComponent<CinemachineVirtualCamera>().
+			GetCinemachineComponent<CinemachineFramingTransposer>();
+
+		defaultCameraDistance = framingTransposer.m_CameraDistance;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		vignette.center.value = Camera.main.WorldToViewportPoint(playerModel.position);
+		if(framingTransposer == null)
+		{
+			return; //only execute update after initialize finished
+		}
+
+		vignette.center.value = Camera.main.WorldToViewportPoint(VignetteCenter.position);
 
 		float vignetteIntensity = CalculateVignetteIntensity();
-		vignette.intensity.value = vignetteIntensity;
+		vignette.intensity.value = Mathf.Min(vignetteIntensity, maxVignetteIntensity);
 		playerController.UpdateLightEffect(vignetteIntensity);
+
+		if(zoom)
+		{
+			framingTransposer.m_CameraDistance = Mathf.Lerp(framingTransposer.m_CameraDistance, cameraMinDistance, Time.deltaTime * zoomSpeed);
+		}
+		else
+		{
+			framingTransposer.m_CameraDistance = Mathf.Lerp(framingTransposer.m_CameraDistance, defaultCameraDistance, Time.deltaTime * reverseZoomSpeed);
+		}
 	}
 
 	float CalculateVignetteIntensity()
@@ -74,10 +112,26 @@ public class VignetteController : MonoBehaviour
 
 		if(contributingLights > 0)
 		{
+			if(!zoom)
+			{
+				waitForZoomTimer += Time.deltaTime;
+
+				if(waitForZoomTimer >= waitForZoomTime)
+				{
+					zoom = true;
+				}
+			}
+
 			return intensity / contributingLights;
 		}
 		else
 		{
+			if(zoom)
+			{
+				zoom = false;
+				waitForZoomTimer = 0.0f;
+			}
+
 			return 0;
 		}
 	}
