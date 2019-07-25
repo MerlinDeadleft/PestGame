c82aed76dbfb76ed79ad1de616d848efa07ea1b4
Kevin Rose <kevin.rose247@gmail.com>
2019-07-25 03:54:10 +0200

    Game can be won Boss spawns after picking up potion

---
+++ Pest/Assets/Scripts/UI/EndSceneUIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndSceneUIController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void PlayAgain()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene("Blocking_Kanalisation");
+	}
+
+	public void Leave()
+	{
+		Application.Quit();
+	}
+}
