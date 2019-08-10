f35220d30e3e7859973cf50d14f178e90cbd4b1b
Kevin Rose <kevin.rose247@gmail.com>
2019-08-11 00:35:21 +0200

    New audio system

---
+++ Pest/Assets/Scripts/Audio/AudioClipDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AudioClipDatabase", menuName = "AudioClipDatabase")]
+public class AudioClipDatabase : ScriptableObject
+{
+	[System.Serializable]
+	public struct AudioClipData
+	{
+		public string Name;
+		public AudioClip AudioClip;
+	}
+
+	public string name = "";
+	public AudioClipData[] AudioClips;
+
+	public AudioClip GetAudioClip(string name)
+	{
+		foreach(AudioClipData clipData in AudioClips)
+		{
+			if(clipData.Name == name)
+			{
+				return clipData.AudioClip;
+			}
+		}
+
+		Debug.Log("No Audio Clip with AudioClipData name: \"" + name + "\" found.");
+		return null;
+	}
+}
